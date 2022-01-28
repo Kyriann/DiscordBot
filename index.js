ebde4728782af68ca247058e11eb48d2e151947a
Kyriann <kyrian.villette27@gmail.com>
2022-01-28 16:48:35 +0100

    V2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const fs = require("fs");
 
-const client = new Discord.Client({presence: {status: 'online',afk: false,activities: [{name: "!lic help",type: "WATCHING"}],} ,intents: ["GUILDS", "GUILD_MESSAGES"]});
+const client = new Discord.Client({presence: {status: 'online',afk: false,activities: [{name: "!lic help",type: "WATCHING"}],} ,intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"]});
 const prefix = process.env.DISCORD_PREFIX;
 
 client.commands = new Discord.Collection();
@@ -21,17 +21,19 @@ client.on('guildMemberAdd', guildMember => {
     guildMember.guild.channels.cache.get('936270551364800632').send(`Bienvenue <@${guildMember.user.id}> dans le gang des Licornes chacal !.`)
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
 
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-   
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
 
-
     if (command === 'ping'){
         client.commands.get('ping').execute(message, args);
+    }else if (command === 'sondage'){
+        client.commands.get('sondage').execute(message, args);
+    }else if (command === 'jeux'){
+        client.commands.get('jeux').execute(message, args);
     }
     
 })
